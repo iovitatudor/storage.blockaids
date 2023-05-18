7d62a9465295ea5e98967ee777f3aad6cae0f119
Tudor Iovita <iovitatudor21@gmail.com>
2023-05-18 11:45:36 +0300

    Change http to https

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ function getMyUrl()
     $protocol = (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1')) ? 'https://' : 'http://';
     $server = $_SERVER['SERVER_NAME'];
     $port = $_SERVER['SERVER_PORT'] ? ':'.$_SERVER['SERVER_PORT'] : '';
-    return $protocol.$server.$port;
+    return 'https://'.$server.$port;
 }
 
 if (isset($_GET['id'])) {
