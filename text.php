41b29e1f2cb22414e9413b4616fb154eee2966b8
Tudor Iovita <iovitatudor21@gmail.com>
2023-05-19 16:02:38 +0300

    Add test

--- text.php
+++ text.php
@@ -1,4 +1,4 @@
 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolore doloribus ducimus expedita fuga id maiores non officiis rerum, unde! Eveniet perferendis ut velit. Ea magnam modi perspiciatis quidem tenetur.
 <video autoplay="" crossorigin="anonymous" class="">
-    <source src="https://storage.blockaids.world/collections/%23EQUALIZE%20AIDS%20Orange-Pink%20NFT.png">
+    <source src="https://bafybeifptczw7v4caqptxuh2twl7fjpaf5dlcanwceum5pxotqkchzjbre.ipfs.dweb.link/image.png">
 </video>
